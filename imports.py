deadb7a3e7748ff97d9a2e8f666a37364d21c674
julienberman <julien.d.berman@gmail.com>
2024-11-30 23:13:02 -0500

    milestone_4

--- imports.py
+++ imports.py
@@ -10,3 +10,15 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import seaborn as sns
+
+# machine learning
+from scipy import stats
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_score, cross_val_predict, GridSearchCV, RandomizedSearchCV, KFold, StratifiedKFold
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder, PolynomialFeatures
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.linear_model import LinearRegression, Lasso, LogisticRegression, LogisticRegressionCV
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve, auc, roc_auc_score, precision_score, recall_score, f1_score
